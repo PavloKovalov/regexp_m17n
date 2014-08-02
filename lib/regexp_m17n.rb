37369ba8d1adf4fdcca3280d1a99f04d32d82769
Pavel Kovalyov <sxe.funky@gmail.com>
2014-08-02 10:21:26 +0300

    Fix regexp encoding compatibility

--- lib/regexp_m17n.rb
+++ lib/regexp_m17n.rb
@@ -1,5 +1,11 @@
 module RegexpM17N
   def self.non_empty?(str)
-    str =~ /^.+$/
+    regexp = '^.+$'
+    if Encoding.compatible?(str, regexp) 
+      regexp = Regexp.new(regexp)
+    else
+      regexp = Regexp.new(regexp.encode(str.encoding), Regexp::FIXEDENCODING)
+    end
+    str =~ regexp
   end
 end
